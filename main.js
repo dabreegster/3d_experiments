b8b424e41d599d712850f37eda67e1de2a6e3be0
Dustin Carlino <dabreegster@gmail.com>
2022-12-11 15:15:15 +0000

    Figure out how to call a WASM module

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './style.css'
 import { plainText as osmInput } from './data/tempe_split.osm';
+//import init, { JsStreetNetwork } from "osm2streets-js";
+import { JsStreetNetwork } from "osm2streets-js";
+
+//await init();
 
 console.log(osmInput);
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
-
 import plainText from 'vite-plugin-plain-text';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [
     plainText.default("data"),
+    wasm(),
+    topLevelAwait(),
   ],
 });
